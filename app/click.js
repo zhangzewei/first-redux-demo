3d2552bc296699089a1f333d1846150f5f64e56f
zhangzewei <zhangzewei@testbird.com>
2016-04-14 20:59:09 +0800

    保存一下

--- app/click.js
+++ app/click.js
@@ -1,26 +1,35 @@
 import React, { Component, PropTypes } from 'react'
 
 class Click extends Component{
-  // constructor(props) {
-  //   super(props)
-  //   this.beClicked = this.beClicked.bind(this)
-  // },
-  // beClicked() {
-  //
-  // },
+  constructor(props) {
+    super(props)
+    this.beClickedTwice = this.beClickedTwice.bind(this)
+  }
+
+  beClickedTwice() {
+    for ( let i = 0; i < 2; i++ ){
+      this.props.beClicked()
+    }
+  }
+
   render() {
-    const { value, beClicked } = this.props;
+    const { value, beClicked, beClickedThird, clearAll} = this.props;
     return (
       <span>
         被点击了：{value} 次
         <button onClick = {beClicked}>点击</button>
+        <button onClick = {this.beClickedTwice}>点击+2</button>
+        <button onClick = {beClickedThird}>点击+3</button>
+        <button onClick = {clearAll}>点击清零</button>
       </span>
     )
   }
 }
 Click.propTypes = {
   value: PropTypes.number.isRequired,
-  onClick: PropTypes.func.isRequired
+  beClicked: PropTypes.func.isRequired,
+  beClickedThird: PropTypes.func.isRequired,
+  clearAll: PropTypes.func.isRequired
 }
 
 export default Click
